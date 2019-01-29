170ef7f22437ed2b550f37c5c2ed7a0b6fb9f109
lanminyuan <45562556+lanminyuan@users.noreply.github.com>
2019-01-29 23:54:57 +0800

    计算器完整版
    计算器的完整版，

--- Calculator.java
+++ Calculator.java
@@ -1,71 +1,145 @@
-package jsq;
+package calculator;
 import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.GridLayout;
 import java.awt.Panel;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.awt.util.ArrayList;
-import java.awt.util.List;
-import java.util.function.IntConsumer;
 
-public class Calculator {
-	private TextField textField;
-	private int firstNum;
-	private final ArrayList fh=new ArrayList();
-			
-			
+import javax.swing.WindowConstants;
 
-	public Calculator() {
-		initPanel();
-		// TODO 自动生成的构造函数存根
+public class Calculator extends WindowAdapter{
+	//定义3个面板
+	Panel p1=new Panel();		
+	Panel p2=new Panel();		
+	Panel p3=new Panel();       
+	TextField txt;				
+	private Button[] b=new Button[17];
+	private String ss[]={"7","8","9","+","4","5","6","-","1","2","3","*","清空","0","=","/","关闭"};
+	static double a=0.0;    
+	static String s,str,s1=" "; 
+	public static void main(String[] args) throws Exception {
+		(new Calculator()).frame();
 	}
-
-	private void initPanel() {
-		Frame frame=new Frame("计算器");
-		frame.setLocationRelativeTo(null);
-		Panel northPanel=new Panel();
-		textField=new TextField(30);
-		northPanel.add(textField);
-		frame.add(northPanel, BorderLayout.NORTH);
-		Panel southPanel=new Panel();
-		GridLayout gridLayout=new GridLayout(3,5,4,4);
-		southPanel.setLayout(gridLayout);
-		String [] Str_Name= {"0","1","2","3","4","5","6","7","8","9","+","-","*","/","="};
-		for(int i=0;i<15;i++) {
-			final Button tmpButton=new Button(Str_Name[i]);
-			tmpButton.addActionListener(new ActionListener() {
-				
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					String text=textField.getText();
-					String buttonLable=tmpButton.getLabel();
-					if(buttonLable.contains("+")||buttonLable.contains("-")||buttonLable.contains("*")||buttonLable.contains("/")){
-						fh.add(buttonLable);
-						firstNum=Integer.valueOf(text);
-						textField.setText("");
-					}else if(buttonLable.contains("=")) {
-						int secondNum=Integer.valueOf(text);
-			}
-					
-					// TODO 自动生成的方法存根
-					
-				}
-			});
+	public void frame() {
+		Frame fm=new Frame("计算器");  
+		for(int i=0;i<=16;i++) {
+			b[i]=new Button(ss[i]); 
+		}
+		for(int i=0;i<=15;i++){
+			p2.add(b[i]);			
 		}
 		
-		
-		// TODO 自动生成的方法存根
+		txt=new TextField(10);
+		txt.setEditable(false);
+		for(int i=0;i<=16;i++) {
+			b[i].addActionListener(new buttonlistener());
+		}
+		//b[16].addActionListener(new close()); 
+		fm.addWindowListener(this);
+		fm.setBackground(Color.red);
+		p1.setLayout(new BorderLayout());
+		p1.add(txt, "Center");
+		p2.setLayout(new GridLayout(4,4,10,10));//设置面板p2布局管理器
+		p3.setLayout(new BorderLayout(10,10)); //设置面板p3布局管理器
+		p3.add(b[16]); //添加按钮到面板p3
+		//添加各个面板到窗口
+		fm.add(p1,"North");
+		fm.add(p2, "Center");
+		fm.add(p3, "South");
+		fm.pack();
+		fm.setVisible(true);
+		fm.setSize(400,750);
+		fm.setTitle("仙女的计算器");
 		
 	}
-
-	public static void main(String[] args) {
-		// TODO 自动生成的方法存根
-
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);   
 	}
-
-}
+	
+	class buttonlistener implements ActionListener {
+		public void actionPerformed(ActionEvent e){
+			Button btn=(Button)e.getSource(); //获取发生事件按钮
+			if(btn.getLabel()=="="){
+				jisuan();
+				str=String.valueOf(a);
+				txt.setText(str);
+				s="";
+				s1="";
+			}else if(btn.getLabel()=="+") {
+				jisuan();
+				txt.setText("");
+				s="+";
+				s1="";
+			}else if(btn.getLabel()=="-"){
+				jisuan();
+				txt.setText("");
+				s="-";
+				s1="";
+			}else if(btn.getLabel()=="*"){
+				jisuan();
+				txt.setText("");
+				s="*";
+				s1="";
+			}else if(btn.getLabel()=="/"){
+				jisuan();
+				txt.setText("");
+				s="/";
+				s1="";
+			}else if(btn.getLabel()=="0"){
+				s1+=btn.getLabel();
+				txt.setText(s1);
+			}else if(btn.getLabel()=="1"){
+				s1+=btn.getLabel();
+				txt.setText(s1);
+			}else if(btn.getLabel()=="2"){
+				s1+=btn.getLabel();
+				txt.setText(s1);
+			}else if(btn.getLabel()=="3"){
+				txt.setText("3");
+			}else if(btn.getLabel()=="4"){
+				s1+=btn.getLabel();
+				txt.setText(s1);
+			}else if(btn.getLabel()=="5"){
+				s1+=btn.getLabel();
+				txt.setText(s1);
+			}else if(btn.getLabel()=="6"){
+				s1+=btn.getLabel();
+				txt.setText(s1);
+			}else if(btn.getLabel()=="7"){
+				s1+=btn.getLabel();
+				txt.setText(s1);
+			}else if(btn.getLabel()=="8"){
+				s1+=btn.getLabel();
+				txt.setText(s1);
+			}
+			else if(btn.getLabel()=="9"){
+				s1+=btn.getLabel();
+				txt.setText(s1);
+			}else {
+				txt.setText("0");
+				s1="";
+			}
+				
+		}
+		public void jisuan(){
+			if(s=="+")
+				a+=Double.parseDouble(txt.getText());
+			else if(s=="-")
+				a-=Double.parseDouble(txt.getText());
+			else if(s=="*")
+				a*=Double.parseDouble(txt.getText());
+			else if(s=="/")
+				a/=Double.parseDouble(txt.getText());
+			else
+				a=Double.parseDouble(txt.getText());
+		}
+	}
+ 
+ }
